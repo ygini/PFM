130f6165ac466c4ea48d87d708b3e6144e2410ec
Yoann Gini <yoann.gini@gmail.com>
2017-07-16 08:29:33 -0400

    Initial Commit

---
+++ PFM/PFM.h
@@ -0,0 +1,19 @@
+//
+//  PFM.h
+//  PFM
+//
+//  Created by Yoann Gini on 16/07/2017.
+//  Copyright © 2017 Yoann Gini. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PFM.
+FOUNDATION_EXPORT double PFMVersionNumber;
+
+//! Project version string for PFM.
+FOUNDATION_EXPORT const unsigned char PFMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PFM/PublicHeader.h>
+
+
